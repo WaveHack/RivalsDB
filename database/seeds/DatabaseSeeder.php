3b3ebeb0e3ec3b8b730b16024be63ede4ecf3b47
WaveHack <email@wavehack.net>
2018-12-15 00:32:30 +0100

    Add db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        \App\Models\User::create([
+            'name' => 'John Doe',
+            'email' => 'email@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $gdiId = \App\Models\Faction::insertGetId([
+            'slug' => 'gdi',
+            'name' => 'GDI',
+        ]);
+
+        \App\Models\Commander::create([
+            'faction_id' => $gdiId,
+            'slug' => 'lt-strongarm',
+            'name' => 'Lt. Strongarm',
+//            'description' => 'placeholder',
+            'rarity' => 'rare',
+            'unlocked_at_level' => 1,
+            'base_health' => 30000,
+            'harvester_health' => 3400,
+            'commander_power_name' => 'Minigun Turret',
+            'commander_power_description' => '',
+            'commander_power_cost' => 40,
+        ]);
+
+
     }
 }
