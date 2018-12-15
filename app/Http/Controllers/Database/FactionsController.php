22c1c04cd0ce2240b5e3396221ec029e9fd3bb28
WaveHack <email@wavehack.net>
2018-12-15 20:13:03 +0100

    Add faction show controller method

--- app/Http/Controllers/Database/FactionsController.php
+++ app/Http/Controllers/Database/FactionsController.php
@@ -9,11 +9,24 @@ class FactionsController extends Controller
 {
     public function index()
     {
-        return view('pages.database.factions', [
+        return view('pages.database.factions.index', [
             'factions' => Faction::query()
                 ->withCount(['commanders', 'units'])
                 ->orderBy('name')
                 ->get(),
         ]);
+    }
+
+    public function show(string $slug)
+    {
+        $faction = Faction::query()
+            ->where('slug', $slug)
+            ->first();
+
+        if (!$faction) {
+            abort(404);
+        }
+
+        return view('pages.database.factions.show', compact('faction'));
     }
 }
