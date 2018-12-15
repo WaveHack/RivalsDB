32403d28e86f508c66c96dbf0c647ea7d4b70a7d
WaveHack <email@wavehack.net>
2018-12-15 20:40:25 +0100

    Add link to units from navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
                             <div class="dropdown-menu">
                                 <a href="{{ route('database.commanders') }}" class="dropdown-item {{ Route::is('database.commanders*') ? 'active' : null }}">Commanders</a>
                                 <a href="{{ route('database.factions') }}" class="dropdown-item {{ Route::is('database.factions*') ? 'active' : null }}">Factions</a>
-                                <a href="#" class="dropdown-item">Units</a>
+                                <a href="{{ route('database.units') }}" class="dropdown-item {{ Route::is('database.units*') ? 'active' : null }}">Units</a>
                                 {{--<div class="dropdown-divider"></div>--}}
                                 {{--<a href="#" class="dropdown-item">Leagues</a>--}}
                                 {{--<a href="#" class="dropdown-item">Maps</a>--}}
