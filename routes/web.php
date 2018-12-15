3d8377b365004b356539b57ef8cb15f13decaf75
WaveHack <email@wavehack.net>
2018-12-15 01:26:45 +0100

    Update home route and controller

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,10 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 
-$router->get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['verify' => true]);
 
+$router->get('/', 'HomeController@index')->name('home');
+
 $router->group(['middleware' => 'verified'], function (Router $router) {
-
-    $router->get('/home', 'HomeController@index')->name('home');
-
+    //
 });
