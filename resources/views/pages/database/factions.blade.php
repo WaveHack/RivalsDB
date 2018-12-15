5d86409e19167effc05b981048edab14cc91e6e5
WaveHack <email@wavehack.net>
2018-12-15 19:48:20 +0100

    Cleanup factions view

--- resources/views/pages/database/factions.blade.php
+++ resources/views/pages/database/factions.blade.php
@@ -12,7 +12,6 @@
             <div class="col-xs-12 col-md-8 offset-md-2 text-center">
                 <h1>Factions</h1>
                 <div class="card-deck">
-
                     @foreach ($factions as $faction)
                         <div class="card">
                             <div class="card-img-top" style="background: url('/assets/images/logos/factions/{{ $faction->slug }}.png') center; background-size: cover; height:128px"></div>
@@ -33,39 +32,6 @@
                             </div>
                         </div>
                     @endforeach
-
-                    {{--<div class="card">
-                        <img src="/assets/images/gdi.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">GDI</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Global Defense Initiative</h6>
-                            <p class="card-text">The Global Defense Initiative is the global government of Earth. It was founded in accordance with the United Nations Global Defense Act (UNGDA), on the date of 12 October 1995, as a united military force for global peacekeeping.</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <a href="#">Commanders</a>
-                                <a href="#">Decks</a>
-                                <a href="#">Units</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card">
-                        <img src="/assets/images/nod.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Nod</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Brotherhood of Nod</h6>
-                            <p class="card-text">The Brotherhood of Nod was a popular, global, religiously developed movement devoted to the guidance of the elusive and charismatic figure of Kane, and the extraterrestrial Tiberium substance that arrived on Earth in 1995.</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <a href="#">Commanders</a>
-                                <a href="#">Decks</a>
-                                <a href="#">Units</a>
-                            </div>
-                        </div>
-                    </div>--}}
-
                 </div>
             </div>
         </div>
