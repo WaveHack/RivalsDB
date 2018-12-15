b15ffdf1a57bd8d5e0022f32b9a746c7016e52cf
WaveHack <email@wavehack.net>
2018-12-15 19:49:26 +0100

    Refactor

--- app/Http/Controllers/Database/FactionsController.php
+++ app/Http/Controllers/Database/FactionsController.php
@@ -10,7 +10,9 @@ class FactionsController extends Controller
     public function index()
     {
         return view('pages.database.factions', [
-            'factions' => Faction::orderBy('name')->get(),
+            'factions' => Faction::query()
+                ->orderBy('name')
+                ->get(),
         ]);
     }
 }
