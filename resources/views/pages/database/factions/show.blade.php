8f33006f66068dbf1c7efcea4af0e4fd36e917e5
WaveHack <email@wavehack.net>
2018-12-15 20:30:48 +0100

    Add factions show page

---
+++ resources/views/pages/database/factions/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center my-3">
+            <div class="col-md-6">
+                @include('partials.global-search')
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-8 offset-md-2">
+                <img src="/assets/images/logos/factions/{{ $faction->slug }}.png" alt="{{ $faction->name }} Logo" style="width: 128px;" class="float-right ml-2">
+                <h1>
+                    <img src="/assets/images/icons/factions/{{ $faction->slug }}.png" alt="{{ $faction->name }} Icon" style="width: 36px;">
+                    {{ $faction->full_name }}
+                    ({{ $faction->name }})
+                </h1>
+                <p>{{ $faction->description }}</p>
+            </div>
+        </div>
+    </div>
+@endsection
