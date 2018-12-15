1e76e9483ea56902d3993917afdf4be7f395683a
WaveHack <email@wavehack.net>
2018-12-15 20:51:19 +0100

    Move commander index view and add show controller method

--- app/Http/Controllers/Database/CommandersController.php
+++ app/Http/Controllers/Database/CommandersController.php
@@ -9,11 +9,25 @@ class CommandersController extends Controller
 {
     public function index()
     {
-        return view('pages.database.commanders', [
+        return view('pages.database.commanders.index', [
             'commanders' => Commander::query()
                 ->with('faction')
                 ->orderBy('unlocked_at_level')
                 ->get(),
         ]);
+    }
+
+    public function show(string $slug)
+    {
+        $commander = Commander::query()
+            ->with('faction')
+            ->where('slug', $slug)
+            ->first();
+
+        if (!$commander) {
+            abort(404);
+        }
+
+        return view('pages.database.commanders.show', compact('commander'));
     }
 }
