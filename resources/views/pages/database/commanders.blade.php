8aaab1b59cd89f2e67c4847f5933e3adb3b4f3ee
WaveHack <email@wavehack.net>
2018-12-15 20:05:30 +0100

    Cleanup commander cards

--- resources/views/pages/database/commanders.blade.php
+++ resources/views/pages/database/commanders.blade.php
@@ -11,36 +11,40 @@
         <div class="row">
             <div class="col-xs-12 col-md-8 offset-md-2 text-center">
                 <h1>Commanders</h1>
-                <div class="card-deck">
-                    @foreach ($commanders as $commander)
-                        <div class="card">
-                            @php($imgPath = "assets/images/logos/commanders/{$commander->slug}.png")
+                @foreach ($commanders->chunk(2) as $commanderChunked)
+                    <div class="card-deck">
+                        @foreach ($commanderChunked as $commander)
+                            <div class="card">
+                                @php($imgPath = "assets/images/logos/commanders/{$commander->slug}.png")
 
-                            @if (file_exists(public_path($imgPath)))
-                                <div class="card-img-top" style="background: url('/{{ $imgPath }}') center; background-size: cover; height:128px"></div>
-                            @endif
+                                @if (file_exists(public_path($imgPath)))
+                                    <div class="card-img-top" style="background: url('/{{ $imgPath }}') center; background-size: cover; height:128px"></div>
+                                @endif
 
-                            <div class="card-body text-left">
-                                <h5 class="card-title">
-                                    <img src="/assets/images/icons/factions/{{ $commander->faction->slug }}.png" style="width: 24px;" class="float-right">
-                                    {{ $commander->name }}
-                                </h5>
-                                <h6 class="card-subtitle mb-2 text-muted">{{ $commander->faction->full_name }}</h6>
-                                <p class="card-text">
-                                    {{ $commander->description }}
-                                </p>
-                                <p class="card-text">
-                                    <strong>Commander Power:</strong> {{ $commander->commander_power_name }}<br>
-                                    Tiberium Cost: {{ $commander->commander_power_cost }}
-                                </p>
-                                <p class="card-text">
-                                    <em>{{ $commander->commander_power_description }}</em>
-                                </p>
+                                <div class="card-body text-left">
+                                    <h5 class="card-title">
+                                        <img src="/assets/images/icons/factions/{{ $commander->faction->slug }}.png" style="width: 24px;" class="float-right">
+                                        {{ $commander->name }}
+                                    </h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">
+                                        Rare Commander
+                                        <span class="float-right">Level {{ $commander->unlocked_at_level }}</span>
+                                    </h6>
+                                    <p class="card-text">
+                                        {{ $commander->description }}
+                                    </p>
+                                    {{--<p class="card-text">
+                                        <strong>Commander Power:</strong> {{ $commander->commander_power_name }}<br>
+                                        Tiberium Cost: {{ $commander->commander_power_cost }}
+                                    </p>
+                                    <p class="card-text">
+                                        <em>{{ $commander->commander_power_description }}</em>
+                                    </p>--}}
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
-                    <div class="card"></div>
-                </div>
+                        @endforeach
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
