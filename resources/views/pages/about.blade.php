aca51f144217d384df60bbd0032cd83e269a907d
WaveHack <email@wavehack.net>
2018-12-16 16:32:49 +0100

    Add target blank to links on about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-sm-12 col-md-8 offset-md-2">
                 <h1>About</h1>
-                <p>RivalsDB is a website with (at the moment) information about Commanders, Factions and Units found in the mobile game <a href="https://www.ea.com/games/command-and-conquer/command-and-conquer-rivals">Command and Conquer: Rivals</a>.</p>
+                <p>RivalsDB is a website with (at the moment) information about Commanders, Factions and Units found in the mobile game <a href="https://www.ea.com/games/command-and-conquer/command-and-conquer-rivals" target="_blank">Command and Conquer: Rivals</a>.</p>
                 <p>The project is still in early development, but new features have been planned, including:</p>
                 <ul>
                     <li>Information about leagues, rewards and maps,</li>
@@ -24,7 +24,7 @@
                     <li>Map creation system, where you can create, share, rate and comment custom maps (as suggestions to the devs)</li>
                     <li>A better layout/theme, including dark mode!</li>
                 </ul>
-                <p>RivalsDB is open source software and can be found on GitHub: <a href="https://github.com/WaveHack/RivalsDB">https://github.com/WaveHack/RivalsDB</a></p>
+                <p>RivalsDB is open source software and can be found on GitHub: <a href="https://github.com/WaveHack/RivalsDB" target="_blank">https://github.com/WaveHack/RivalsDB</a></p>
             </div>
         </div>
     </div>
