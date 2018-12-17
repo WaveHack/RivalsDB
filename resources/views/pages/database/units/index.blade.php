f753470d2ddc8ddecc2fadf7af945966973cb5f9
WaveHack <email@wavehack.net>
2018-12-17 22:39:14 +0100

    Add alt and title to faction logos on unit cards

--- resources/views/pages/database/units/index.blade.php
+++ resources/views/pages/database/units/index.blade.php
@@ -29,7 +29,7 @@
 
                                     <div class="card-body text-left">
                                         <h5 class="card-title">
-                                            <img src="/assets/images/icons/factions/{{ $unit->faction->slug }}.png" style="width: 24px;" class="float-right">
+                                            <img src="/assets/images/icons/factions/{{ $unit->faction->slug }}.png" alt="{{ $unit->faction->name }} Logo" title="{{ $unit->faction->name }}" style="width: 24px;" class="float-right">
                                             <a href="{{ route('db.units.show', $unit->slug) }}">
                                                 {{ $unit->name }}
                                             </a>
