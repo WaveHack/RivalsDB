e3070224d34add715b9862924c41367fc6561415
WaveHack <email@wavehack.net>
2018-12-17 21:18:26 +0100

    Don't limit commander card flavor text

--- resources/views/pages/database/commanders/index.blade.php
+++ resources/views/pages/database/commanders/index.blade.php
@@ -45,7 +45,7 @@
                                     </h6>
 
                                     <p class="card-text">
-                                        <em>{{ str_limit($commander->flavor_description, 200) }}</em>
+                                        <em>{{ $commander->flavor_description }}</em>
                                     </p>
                                 </div>
                             </div>
