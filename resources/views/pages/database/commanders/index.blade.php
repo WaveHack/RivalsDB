6804d739cdd4285528044d7dc612c5733819dc98
WaveHack <email@wavehack.net>
2018-12-17 22:24:34 +0100

    Use css grid instead of card-deck for commanders index

--- resources/views/pages/database/commanders/index.blade.php
+++ resources/views/pages/database/commanders/index.blade.php
@@ -14,44 +14,44 @@
 
                 @if ($commanders->isEmpty())
                     <p>No results found.</p>
-                @endif
+                @else
+                    <div class="row">
+                        @foreach ($commanders as $commander)
+                            <div class="col-12 col-sm-6 col-lg-3">
+                                <div class="card card-rarity-{{ $commander->rarity }} mb-3">
+                                    @php($portraitPath = "assets/images/portraits/commanders/{$commander->slug}-card.jpg")
 
-                @foreach ($commanders->chunk(4) as $commandersChunked)
-                    <div class="card-deck mb-4">
-                        @foreach ($commandersChunked as $commander)
-                            <div class="card card-rarity-{{ $commander->rarity }}">
-                                @php($portraitPath = "assets/images/portraits/commanders/{$commander->slug}-card.jpg")
-
-                                @if (file_exists(public_path($portraitPath)))
-                                    <a href="{{ route('db.commanders.show', $commander->slug) }}">
-                                        <img src="/{{ $portraitPath }}" alt="Portrait of commander {{ $commander->name }}" class="card-img-top">
-                                    </a>
-                                @endif
-
-                                <div class="card-body text-left">
-                                    <h5 class="card-title">
-                                        <img src="/assets/images/icons/factions/{{ $commander->faction->slug }}.png" style="width: 24px;" class="float-right">
+                                    @if (file_exists(public_path($portraitPath)))
                                         <a href="{{ route('db.commanders.show', $commander->slug) }}">
-                                            {{ $commander->name }}
+                                            <img src="/{{ $portraitPath }}" alt="Portrait of commander {{ $commander->name }}" class="card-img-top">
                                         </a>
-                                    </h5>
+                                    @endif
 
-                                    <h6 class="card-subtitle mb-2 text-muted">
-                                        <span class="rarity-{{ $commander->rarity }}">
-                                            {{ ucfirst($commander->rarity) }}
-                                            Commander
-                                        </span>
-                                        {{--<span class="float-right">Level {{ $commander->unlocked_at_level }}</span>--}}
-                                    </h6>
+                                    <div class="card-body text-left">
+                                        <h5 class="card-title">
+                                            <img src="/assets/images/icons/factions/{{ $commander->faction->slug }}.png" style="width: 24px;" class="float-right">
+                                            <a href="{{ route('db.commanders.show', $commander->slug) }}">
+                                                {{ $commander->name }}
+                                            </a>
+                                        </h5>
 
-                                    <p class="card-text">
-                                        <em>{{ $commander->flavor_description }}</em>
-                                    </p>
+                                        <h6 class="card-subtitle mb-2 text-muted">
+                                            <span class="rarity-{{ $commander->rarity }}">
+                                                {{ ucfirst($commander->rarity) }}
+                                                Commander
+                                            </span>
+                                            {{--<span class="float-right">Level {{ $commander->unlocked_at_level }}</span>--}}
+                                        </h6>
+
+                                        <p class="card-text">
+                                            <em>{{ $commander->flavor_description }}</em>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
                     </div>
-                @endforeach
+                @endif
             </div>
         </div>
     </div>
